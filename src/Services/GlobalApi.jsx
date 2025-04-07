bc9ec145411058fe15299ae4206c8e6f15d66a9a
Mark Neal <mark.neal22@gmail.com>
2025-04-07 10:53:41 -0500

    GlobalAPi update

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:1337/api'; // 
+const BASE_URL = 'http://localhost:1337/api';
 
 const getPost=axios.get(BASE_URL + 'articles?populate=*');
 
