a21ad8a19926c3d27326e92ab181b8ac4956ed12
Mark Neal <mark.neal22@gmail.com>
2025-04-07 12:04:07 -0500

    Updated Home

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,42 +1,49 @@
 import React, { useEffect, useState } from 'react'
-import Header from '../Components/Header'
 import Search from '../Components/Search'
 import IntroPost from '../Components/IntroPost'
 import Blogs from '../Components/Blogs'
-import Footer from '../Components/Footer'
 import GlobalApi from '../Services/GlobalApi'
 
 function Home() {
   const [post,setPost]=useState([])
+  const [orgPost,setOrgPost]=useState([])
 
   useEffect(()=>{
-    getPost();
-},[])
-const getPost=()=>{
-    GlobalApi.getPost.then(response=>{
-        const result=response.data.data.map(item=>({
-            id:item.id,
-            title:item.attributes.title,
-            desc:item.attributes.description,
-            tag:item.attributes.tag,
-            coverImage:item.attributes.coverImage.data.attributes.url,
-        }));
-        setPost(result)
-    })
-}
+      getPost();
+  },[])
+  const getPost=()=>{
+      GlobalApi.getPost.then(resp=>{
+          const result=resp.data.data.map(item=>({
+              id:item.id,
+              title:item.attributes.title,
+              desc:item.attributes.description,
+              tag:item.attributes.tag,
+              coverImage:item.attributes.coverImage.data.attributes.url,
+          }));
+          setPost(result);
+          setOrgPost(result);
+      })
+  }
+
+  const filterPost=(tag)=>{
+    if(tag=='All')
+    {
+      setPost(orgPost);
+      return ;
+    }
+    const result=orgPost.filter(item=>item.tag==tag);
+    setPost(result);
+  }
 
   return (
-    <div>
-        {/* <header/> */}
-        <Header/>
-        {/* <search/> */}
-        <Search/>
-        {/* <introPost/> */}
-        {post.length>0? <IntroPost post={post[0]} />:null}
-        {/* <blogs/> */}
-        <Blogs/>
-        {/* <footer/> */}  
-        <Footer/> 
+    <div >
+      {/* Search */}
+      <Search selectedTag={(tag)=>filterPost(tag)} />
+      {/* IntroPost */}
+      {post.length>0? <IntroPost post={post[0]} />:null}
+      {/* Blogs */}
+      {post.length>0?  <Blogs posts={post}/>:null}
+      
     </div>
   )
 }
