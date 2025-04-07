d14daf250ca134ac8204f05274bb25c04b002a4b
Mark Neal <mark.neal22@gmail.com>
2025-04-07 12:03:30 -0500

    Update GlobalApi

--- src/Services/GlobalApi.jsx
+++ src/Services/GlobalApi.jsx
@@ -1,9 +1,11 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:1337/api';
+const BASE_URL = 'http://localhost:1337/api/';
 
 const getPost=axios.get(BASE_URL + 'articles?populate=*');
+const getPostById=(id)=>axios.get(BASE_URL+'/articles/'+id+'?populate=*');
 
 export default(
-    getPost
+    getPost,
+    getPostById
 )
