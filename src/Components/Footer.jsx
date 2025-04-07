a6b55c3810d70140cf91198d2b4e90a4c5c733d7
Mark Neal <mark.neal22@gmail.com>
2025-04-07 12:06:13 -0500

    Updated Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Footer() {
   return (
-    <div>Footer</div>
+    <div className='bg-gray-200 text-center'>
+        <h1 className='text-[14px] p-10'>Need help? Email hello@nebulatimes.com<br></br>
+Copyright © 2025 NebulaTImes</h1>
+    </div>
   )
 }
 
